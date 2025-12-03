8cd8cdf9598ed573668a67b541bf2020240da01a
prab-webknot <prabhanjan.s@webknot.in>
2025-12-03 14:09:09 +0530

    added worker thread and refetchign in the main thread

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -10,18 +11,40 @@ import (
 	@you will lean how the multi thread works here
 */
 
-func Worker(url string) string {
+func Worker(url string, wg *sync.WaitGroup, resultChanel chan string) {
+	defer wg.Done()
 	time.Sleep(50 * time.Millisecond)
 
 	fmt.Println("Url:", url)
-	return url
+
+	resultChanel <- url
+
 }
 
 func main() {
 	startTime := time.Now()
+	var wg sync.WaitGroup                // --> this should be added as worker run in other concurrent than main thread
+	resultChanel := make(chan string, 3) // -> need to pass the chanel as worker will pass the data to channel and from channel we will received data to main thread
 
-	result := Worker("don.jpg")
-	fmt.Println("this is result:", result)
+	/*
+		GO Routine life cycle
+		fan out --> main thread to worker/ ( light weight thread )
+		fan in --> worker thread to main thread
+
+		main --> worker --> worker to channel --> channel to main thead
+	*/
+
+	wg.Add(3)                               // --> need to pass how many thread we are running as worker
+	go Worker("don.jpg", &wg, resultChanel) // --> need to pass as the refer &wg
+	go Worker("don1.jpg", &wg, resultChanel)
+	go Worker("don2.jpg", &wg, resultChanel)
+
+	wg.Wait()
+
+	close(resultChanel)
+	for result := range resultChanel {
+		print("received \n:", result)
+	}
 
 	elapsed := time.Since(startTime)
 	fmt.Printf("took time: %d ms\n", elapsed.Milliseconds())
